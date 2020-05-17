91e5930b4deed66bf62112ede65f311850e9224f
KishenSharma6 <kishen.r.sharma@gmail.com>
2020-05-16 19:43:07 -0700

    Minor edits

--- Project Codes/04_Modeling/TimeSeriesStats.py
+++ Project Codes/04_Modeling/TimeSeriesStats.py
@@ -4,8 +4,9 @@ import numpy as np
 #Metrics to evaluate time series forecasts
 def model_performance(predictions, actual):
     errors = predictions - actual
+    mape = np.mean(np.abs(errors)/np.abs(actual))
     mse = np.square(errors).mean()
     rmse = np.sqrt(mse)
     mae = np.abs(errors).mean()
-    metrics = {'MSE': mse, 'RMSE':rmse, 'MAE': mae}
+    metrics = {'MAE': mae,'MAPE':mape,'MSE': mse, 'RMSE':rmse}
     return(metrics)
