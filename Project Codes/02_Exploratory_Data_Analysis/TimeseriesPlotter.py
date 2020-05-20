fa875b7b8b7e476d328dc7bbadf247c726687ac4
KishenSharma6 <kishen.r.sharma@gmail.com>
2020-05-19 17:08:31 -0700

    Through eod

--- Project Codes/02_Exploratory_Data_Analysis/TimeseriesPlotter.py
+++ Project Codes/02_Exploratory_Data_Analysis/TimeseriesPlotter.py
@@ -1,13 +1,13 @@
 #Function plots time series data
-def timeseries_plot(series, figsize = None , title = None, xlabel = None, ylabel = None,legend = False,label = None,
+def timeseries_plot(series, title = None, xlabel = None, ylabel = None,legend = False,label = None,
                     title_dict = None, label_dict = None,color = None,ax=None, alpha = None):
     """
     Plot Time Series Data
     """
-    plot = series.plot(color = color,label = label,alpha=alpha, ax=ax)
+    plot = series.plot(color = color,label = label,alpha=alpha,ax=ax)
     plot.set_title(title, fontdict=title_dict)
     plot.set_ylabel(ylabel,fontdict=label_dict)
     plot.set_xlabel(xlabel,fontdict=label_dict)
     if legend == True:
-        plot.legend(fancybox = True, shadow = True, frameon = True)
+        plot.legend(label=label,fancybox = True, shadow = True, frameon = True)
     return plot
