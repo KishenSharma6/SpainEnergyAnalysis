84e3f3d776d7a65e9c573595cf2693a3b3c3db81
KishenSharma6 <kishen.r.sharma@gmail.com>
2020-06-25 09:57:00 -0700

    Added print and return argument to metrics function

--- Project_Codes/04_Modeling/TimeSeriesStats.py
+++ Project_Codes/04_Modeling/TimeSeriesStats.py
@@ -2,11 +2,17 @@
 import numpy as np
 
 #Metrics to evaluate time series forecasts
-def model_performance(predictions, actual):
+def model_performance(predictions, actual, model_name, return_metrics = True):
     errors = predictions - actual
     mape = np.mean(np.abs(errors)/np.abs(actual))
     mse = np.square(errors).mean()
     rmse = np.sqrt(mse)
     mae = np.abs(errors).mean()
     metrics = {'MAE': mae,'MAPE':mape,'MSE': mse, 'RMSE':rmse}
-    return(metrics)
+    print(model_name,'Performance Metrics')
+    print('=======================================')
+    print('MAE:{}\nMAPE:{}\nMSE:{}\nRMSE:{}'.format(metrics['MAE'],metrics['MAPE'],metrics['MSE'],metrics['RMSE']))
+    if return_metrics == True:
+        return(metrics)
+        
+    
