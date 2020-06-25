6049edf557eb763c9707e39703026c37c3a57f8e
KishenSharma6 <kishen.r.sharma@gmail.com>
2020-06-25 09:56:10 -0700

    Removed legend srguments from plotting function

--- Project_Codes/03_Data_Processing/TimeseriesPlotter.py
+++ Project_Codes/03_Data_Processing/TimeseriesPlotter.py
@@ -1,5 +1,5 @@
 #Function plots time series data
-def timeseries_plot(series, figsize = None , title = None, xlabel = None, ylabel = None,legend = False,label = None,
+def timeseries_plot(series, figsize = None , title = None, xlabel = None, ylabel = None,
                     title_dict = None, label_dict = None,color = None,ax=None, alpha = None):
     """
     Plot Time Series Data
@@ -8,6 +8,4 @@ def timeseries_plot(series, figsize = None , title = None, xlabel = None, ylabel
     plot.set_title(title, fontdict=title_dict)
     plot.set_ylabel(ylabel,fontdict=label_dict)
     plot.set_xlabel(xlabel,fontdict=label_dict)
-    if legend == True:
-        plot.legend(fancybox = True, shadow = True, frameon = True)
     return plot
