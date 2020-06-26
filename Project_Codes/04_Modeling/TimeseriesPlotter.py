592d4056e3b9ead3798fb16819890a482e08ee4e
KishenSharma6 <kishen.r.sharma@gmail.com>
2020-06-26 12:59:18 -0700

    Small changes but bug emerged in 04_modeling file

--- Project_Codes/04_Modeling/TimeseriesPlotter.py
+++ Project_Codes/04_Modeling/TimeseriesPlotter.py
@@ -1,12 +1,15 @@
 #Function plots time series data
-def timeseries_plot(series, figsize = None , title = None, xlabel = None, ylabel = None,legend = False,label = None,title_dict = None, label_dict = None,color = None,ax=None, alpha = None):
+def timeseries_plot(series, figsize = None , title = None, xlabel = None, ylabel = None,
+                    title_dict = None, label_dict = None,color = None,ax=None, alpha = None,
+                   fontsize=None, fontweight = None, style = None):
     """
     Plot Time Series Data
     """
-    plot = series.plot(color = color,label = label,alpha=alpha, ax=ax,figsize = figsize)
-    plot.set_title(title, fontdict=title_dict)
-    plot.set_ylabel(ylabel,fontdict=label_dict)
-    plot.set_xlabel(xlabel,fontdict=label_dict)
-    if legend == True:
-        plot.legend(fancybox = True, shadow = True, frameon = True)
+    plot = series.plot(color = color,alpha=alpha, ax=ax)
+    plot.set_title(title, fontdict=title_dict,fontsize=fontsize, fontweight = fontweight, 
+                   style = style)
+    plot.set_ylabel(ylabel,fontdict=label_dict,fontsize=fontsize, fontweight = fontweight, 
+                    style = style)
+    plot.set_xlabel(xlabel,fontdict=label_dict,fontsize=fontsize, fontweight = fontweight, 
+                    style = style)
     return plot
