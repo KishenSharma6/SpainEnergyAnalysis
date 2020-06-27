56123c8270f68405654733ae226dde4a6fec96de
KishenSharma6 <kishen.r.sharma@gmail.com>
2020-06-27 14:40:58 -0700

    Should resolve plotting issues

--- Project_Codes/03_Data_Processing/TimeseriesPlotter.py
+++ Project_Codes/03_Data_Processing/TimeseriesPlotter.py
@@ -1,15 +1,11 @@
 #Function plots time series data
-def timeseries_plot(series, figsize = None , title = None, xlabel = None, ylabel = None,
-                    title_dict = None, label_dict = None,color = None,ax=None, alpha = None,
-                   fontsize=None, fontweight = None, style = None):
+def timeseries_plot(series, figsize = None , title = None, xlabel = None, ylabel = None,label = None,
+                    title_dict = None, label_dict = None,color = None,ax=None, alpha = None):
     """
     Plot Time Series Data
     """
-    plot = series.plot(color = color,alpha=alpha, ax=ax)
-    plot.set_title(title, fontdict=title_dict,fontsize=fontsize, fontweight = fontweight, 
-                   style = style)
-    plot.set_ylabel(ylabel,fontdict=label_dict,fontsize=fontsize, fontweight = fontweight, 
-                    style = style)
-    plot.set_xlabel(xlabel,fontdict=label_dict,fontsize=fontsize, fontweight = fontweight, 
-                    style = style)
+    plot = series.plot(color = color,label = label,alpha=alpha, ax=ax, figsize = figsize)
+    plot.set_title(title, fontdict=title_dict)
+    plot.set_ylabel(ylabel,fontdict=label_dict)
+    plot.set_xlabel(xlabel,fontdict=label_dict)
     return plot
