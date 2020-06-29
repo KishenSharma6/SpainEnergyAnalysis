a5b9627a882f710a10921df5715edee9f5bdad28
KishenSharma6 <kishen.r.sharma@gmail.com>
2020-06-29 12:50:21 -0700

    Updated graphing function

--- Project_Codes/04_Modeling/TimeSeriesStats.py
+++ Project_Codes/04_Modeling/TimeSeriesStats.py
@@ -4,14 +4,14 @@ import numpy as np
 #Metrics to evaluate time series forecasts
 def model_performance(predictions, actual, model_name, return_metrics = True):
     errors = predictions - actual
-    mape = np.mean(np.abs(errors)/np.abs(actual))
+    mape = np.mean(np.abs(errors)/np.abs(actual)) * 100
     mse = np.square(errors).mean()
     rmse = np.sqrt(mse)
     mae = np.abs(errors).mean()
     metrics = {'MAE': mae,'MAPE':mape,'MSE': mse, 'RMSE':rmse}
     print(model_name,'Performance Metrics')
     print('=======================================')
-    print('MAE:{}\nMAPE:{}\nMSE:{}\nRMSE:{}'.format(metrics['MAE'],metrics['MAPE'],metrics['MSE'],metrics['RMSE']))
+    print('MAE:{}\nMAPE:{}%\nMSE:{}\nRMSE:{}'.format(metrics['MAE'],metrics['MAPE'],metrics['MSE'],metrics['RMSE']))
     if return_metrics == True:
         return(metrics)
         
